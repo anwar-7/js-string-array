842a1db0338c0b29b8861485af56900d43149013
anwarul1 <anwarjewel007@gmail.com>
2022-02-14 02:31:12 +0600

    case convert end

---
+++ case-convert.js
@@ -0,0 +1,11 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+
+const userInput = 'blAckPinK';
+const savedUserName = 'blackPink';
+if (userInput.toLowerCase() == savedUserName.toLowerCase()) {
+  console.log('user exists');
+}
+
+// toLowerCase() make string into lower case
+// toUpperCase() make string into upper case
